e7cfa1248b69e05690528cc6bcfe1a57022f322c
Java-couchpotato <64412019+Java-couchpotato@users.noreply.github.com>
2022-08-24 16:53:32 +0200

    refactor

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,5 @@
 public class Main {
-    //Create a simple Client-Server application
-    //
-    //**Flow:**
-    //
-    //Client sends a number to server (i.e. 5)
-    //
-    //Server returns a randomly generated array of 5 elements
-    //
-    //[4, 1, 2, 5, 3]
-    //
-    //Client prints the resulting array
-    //
-    //*perform data exchange using JDK serialization, instead of strings.
-    //
-    //**Note**: Server should be kept alive all the time
+
     public static void main(String[] args) {
       
     }
